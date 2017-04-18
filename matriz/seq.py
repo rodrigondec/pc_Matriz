d1acbbe2a69efbb3e2ac22b04eed2f2a8f404356
RodrigoNdeC <rodrigondec@gmail.com>
2017-04-18 03:22:59 -0300

    salvando em log os tempos separados de execução, read e print

--- matriz/seq.py
+++ matriz/seq.py
@@ -2,13 +2,15 @@ import time
 from matriz import *
 
 tempo_total = 0
+tempo_read = 0
+tempo_print = 0
 
 start = time.time()
 matrizes = load_matrizes(6)
 end = time.time()
 
 tempo = end - start
-tempo_total += tempo
+tempo_read += tempo
 
 a = matrizes[0]
 b = matrizes[1]
@@ -18,7 +20,7 @@ log = open("log/seq/"+str(len(a))+".txt", "w")
 log.write("read: "+str(tempo)) 
 log.write("\n\n")
 
-for x in range(0, 20):
+for x in range(1, 21):
 
 	start = time.time()
 	multiplicar_matrizes(a, b, resultado)
@@ -27,7 +29,7 @@ for x in range(0, 20):
 	tempo = end - start
 	tempo_total += tempo
 
-	log.write("execute: "+str(tempo)) 
+	log.write("execute "+str(x)+": "+str(tempo)) 
 	log.write("\n")
 
 start = time.time()
@@ -35,7 +37,7 @@ print_matriz(resultado)
 end = time.time()
 
 tempo = end - start
-tempo_total += tempo
+tempo_print += tempo
 
 log.write("\n")
 log.write("print: "+str(tempo)) 
@@ -43,7 +45,9 @@ log.write("\n")
 
 print(tempo_total)
 log.write("\n")
-log.write("Tempo total: "+str(tempo_total))
+log.write("Tempo total de execução: "+str(tempo_total))
 log.write("\n")
-log.write("Tempo medio: "+str(tempo_total/20))
+log.write("Tempo medio de execução: "+str(tempo_total/20))
+log.write("\n")
+log.write("Tempo medio de execução + read + print: "+str((tempo_total/20) + tempo_read + tempo_print))
 log.close()
