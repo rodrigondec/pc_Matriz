baebc2269722024503e9ff34e11bba6b45328027
RodrigoNdeC <rodrigondec@gmail.com>
2017-04-18 00:39:44 -0300

    script salvando tempo total para fazer tempo médio

--- matriz/seq.py
+++ matriz/seq.py
@@ -1,15 +1,22 @@
 import time
 from matriz import *
 
-matrizes = load_matrizes(6)
+matrizes = load_matrizes(5)
 a = matrizes[0]
 b = matrizes[1]
 
 resultado = fazer_matriz(len(a), len(b[0]))
 log = open("log/seq/"+str(len(a))+".txt", "w")
+tempo_total = 0
 for x in range(0, 30):
 	start = time.time()
 	multiplicar_matrizes(a, b, resultado)
 	end = time.time()
-	log.write(str(end - start)) 
+	tempo = end - start
+	tempo_total += tempo
+	log.write(str(tempo)) 
 	log.write("\n")
+
+print(tempo_total)
+log.write("\n")
+log.write("Tempo total: "+str(tempo_total))
